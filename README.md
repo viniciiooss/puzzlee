306008a7b7d9ca3a87bc19aacddeed40fe233029
vinicios <usuario1@MacBook-Pro-de-usuario1.local>
2024-06-09 15:04:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,25 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+![Project Preview](./src/find-images.png)
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### `yarn install`
+
+Instal All dependencies in this project
+
+### `yarn dev`
+
+Runs the app in the development mode.<br />
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+
+### Link
+
+- Unsplash Developer: https://unsplash.com/developers
+- Create React App: https://create-react-app.dev/
+
+### Video Tutorial
+
+You can see my youtube video for this project in [here](https://youtu.be/yUau8xImmK0)
---
+++ src/App.js
@@ -0,0 +1,33 @@
+import { createContext, useState } from "react";
+import Images from "./components/Images";
+import Jumbutron from "./components/Jumbutron";
+import SearchField from "./components/SearchField";
+import useAxios from "./hooks/useAxios";
+
+// Create Context
+export const ImageContext = createContext();
+
+function App() {
+  const [searchImage, setSearchImage] = useState('');
+  const { response, isLoading, error, fetchData } = useAxios(`search/photos?page=1&query=cats&client_id=${process.env.REACT_APP_ACCESS_KEY}`);
+
+  const value = {
+    response,
+    isLoading,
+    error,
+    fetchData,
+    searchImage,
+    setSearchImage
+  }
+
+  return (
+    <ImageContext.Provider value={value}>
+      <Jumbutron>
+        <SearchField />
+      </Jumbutron>
+      <Images />
+    </ImageContext.Provider>
+  );
+}
+
+export default App;
