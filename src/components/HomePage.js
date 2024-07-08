b199fc6059db6f19810bea6e2d8649000ba8d11d
ednaldo123 <ednaldobritojr004@gmail.com>
2024-07-08 16:54:56 -0300

    Estetica

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,17 +8,15 @@ const HomePage = () => {
   return (
     <div className="home-page">
       <h1 className="text-center mt-6 text-2xl">Bem-vindo ao Puzzle e Chatbot</h1>
-      <div className="button-container" style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+      <div className="button-container">
         <button
-          className="button"
-          style={{ marginRight: '20px', padding: '10px 20px', backgroundColor: 'green', color: 'white', fontSize: '16px' }}
+          className="button chatbot-button"
           onClick={() => navigate('/chatbot')}
         >
           Chatbot
         </button>
         <button
-          className="button"
-          style={{ padding: '10px 20px', backgroundColor: 'blue', color: 'white', fontSize: '16px' }}
+          className="button puzzles-button"
           onClick={() => navigate('/puzzles')}
         >
           Puzzles
